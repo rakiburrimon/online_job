cca251c50afad27f093c8b86b2364aecf897bb00
Md. Rakibur Rahman <rakiburrimon@gmail.com>
2018-11-13 16:31:29 +0600

    Update index.php

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#" data-toggle="dropdown" role="button">
+            <a class="nav-link" data-toggle="dropdown" role="button">
               Login
             </a>
           </li>
