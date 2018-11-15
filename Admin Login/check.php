341388863ead24b775b6a013f80bf21a288e92db
Md. Rakibur Rahman <rakiburrimon@gmail.com>
2018-11-15 14:00:21 +0600

    multi
    admin login check.php
    employer login check.php

--- Admin Login/check.php
+++ Admin Login/check.php
@@ -4,6 +4,8 @@ session_start();
 $user_check=$_SESSION['admin_email'];
  
 $sql = mysqli_query($db,"SELECT admin_name FROM admin WHERE admin_email='$user_check' ");
+
+$sql1 = mysqli_query($db,"SELECT admin_id FROM admin WHERE admin_email='$user_check' ");
  
 $row=mysqli_fetch_array($sql,MYSQLI_ASSOC);
  
--- Employer Login/check.php
+++ Employer Login/check.php
@@ -5,6 +5,8 @@ $user_check=$_SESSION['company_name'];
  
 $sql = mysqli_query($db,"SELECT company_name FROM employer WHERE company_name='$user_check' ");
  
+ $sql1 = mysqli_query($db,"SELECT company_id FROM employer WHERE company_name='$user_check' ");
+
 $row=mysqli_fetch_array($sql,MYSQLI_ASSOC);
  
 $login_user=$row['company_name'];
