f68abf90c424ba83039d861339f04d152206d065
Md. Rakibur Rahman <rakiburrimon@gmail.com>
2018-11-17 12:56:00 +0600

    update job post

--- employer_login/insert_job.php
+++ employer_login/insert_job.php
@@ -19,7 +19,7 @@ $a6=$_POST["job_location"];
 $a7=$_POST["job_salary"];
 $a8=$_POST["application_deadline"];
 
-$sql = "INSERT INTO `job` (`job_id`, `job_title`, `job_context`, `job_responsibilities`, `educaqtional_requirement`, `job_experience_required`, `job_status`, `job_location`, `job_salary`, `job_application_deadline`, `employer_id`) VALUES (NULL, '$a1', '$a2', '$a3', '$a4', '$a5', 'Active', '$a6', '$a7', '$a8', NULL)";
+$sql = "INSERT INTO `job` (`job_id`, `job_title`, `job_context`, `job_responsibilities`, `educaqtional_requirement`, `job_experience_required`, `job_status`, `job_location`, `job_salary`, `job_application_deadline`, `employer_id`) VALUES (NULL, '$a1', '$a2', '$a3', '$a4', '$a5', 'Active', '$a6', '$a7', '$a8', '$_SESSION['company_id']')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
