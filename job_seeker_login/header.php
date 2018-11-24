b64da33625a0f35c15893d81ac980e1edfb910d9
Md. Rakibur Rahman <rakiburrimon@gmail.com>
2018-11-24 16:12:31 +0600

    ssss

--- job_seeker_login/header.php
+++ job_seeker_login/header.php
@@ -27,7 +27,7 @@
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="nav navbar-nav ml-auto">
-                            <li class="nav-item active">
+                            <li class="nav-item ">
                                 <a class="nav-link" href="#">Page</a>
                             </li>
                             <li class="nav-item">
