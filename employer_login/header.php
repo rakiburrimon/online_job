db7a7f28c2568d575ae2fa5897ca17a82bf3cdb5
Md. Rakibur Rahman <rakiburrimon@gmail.com>
2018-11-24 13:09:16 +0600

    update employer header

--- employer_login/header.php
+++ employer_login/header.php
@@ -10,6 +10,9 @@
     <!-- Font Awesome JS -->
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
@@ -39,9 +42,18 @@
                         </ul>
                         <ul class="nav navbar-nav navbar-right">
                     <?php if (empty($_SESSION['company_name'])) { ?>
-                        <li><a href="home.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>                        
+                        <li><a href="Login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>                        
                     <?php } else { ?>
-                        <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> <?php echo ($_SESSION['company_name']); ?></a></li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                            <?php echo ($_SESSION['company_name']); ?>
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="logout.php">Logout</a>
+                            <a class="dropdown-item" href="#">Link 2</a>
+                            <a class="dropdown-item" href="#">Link 3</a>
+                        </div>
+                        </li>
                     <?php } ?>
                         
                     
