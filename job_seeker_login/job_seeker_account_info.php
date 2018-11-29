9d2f900e29ea4338caa68e669e0d6ab537f0fa51
Md. Rakibur Rahman <rakiburrimon@gmail.com>
2018-11-29 14:46:41 +0600

    job seeker detail update

---
+++ job_seeker_login/job_seeker_account_info.php
@@ -0,0 +1,132 @@
+<?php 	
+if (!isset($_SESSION)) session_start();
+?>
+			<style>
+				
+			</style>
+
+			<div class="col-md-9 hidden-print">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3>Account Info</h3>
+					</div>
+					<div class="panel-body">
+						<div style="margin: 0 auto;">
+
+							<table class="table table-striped">
+								<tr>
+									<th>Name</th>
+									<th>Email</th>
+									<th>Contact</th>
+									<th>Address</th>
+									<th>Career Objective</th>
+									<th>Gender</th>
+									<th>Image</th>
+									<th></th>
+									<th></th>
+								</tr>
+
+								<?php 
+										include 'connection.php';
+
+										$q = "SELECT * FROM job_seeker Where job_seeker_id = '".$_SESSION['job_seeker_id']."'";
+										
+										$query = mysqli_query($conn,$q);
+
+										while ($res= mysqli_fetch_array($query)) {
+										
+								?>
+
+								<tr>
+									<td><?php echo $res['job_seeker_name']; ?></td>
+									<br>
+									<td><?php echo $res['job_seeker_email']; ?></td>
+									<td><?php echo $res['job_seeker_contact']; ?></td>
+									<td><?php echo $res['job_seeker_address']; ?></td>
+									<td><?php echo $res['job_seeker_career_objective']; ?></td>
+									<td><?php echo $res['job_seeker_gender']; ?></td>
+									<td><?php echo $res['image']; ?></td>
+									<td><a class="btn btn-success" name="update" href="update_job_seeker.php?job_seeker_id=<?php echo $res['job_seeker_id']; ?>">Update</a></td>
+									<td><a class="btn btn-success" href="certificate.php?job_seeker_id=<?php echo $res['job_seeker_id']; ?>">Certificale</a></td>
+
+								</tr>
+
+								<?php 
+									}
+								?>
+																
+							</table>
+							<table class="table table-striped">
+								<tr>
+									<th>Organization</th>
+									<th>Duration</th>
+									<th></th>
+									<th></th>
+								</tr>
+
+								<?php 
+										include 'connection.php';
+
+										$q = "SELECT * FROM experience Where job_seeker_id = '".$_SESSION['job_seeker_id']."'";
+										
+										$query = mysqli_query($conn,$q);
+
+										while ($res= mysqli_fetch_array($query)) {
+										
+								?>
+
+								<tr>
+									<td><?php echo $res['experience_organization']; ?></td>
+									<br>
+									<td><?php echo $res['experience_duration']; ?></td>
+									<td><a class="btn btn-success" name="update" href=" ?job_seeker_id=<?php echo $res['job_seeker_id']; ?>">Update</a></td>
+									<td><a class="btn btn-success" href=" ?job_seeker_id=<?php echo $res['job_seeker_id']; ?>">Delete</a></td>
+
+								</tr>
+
+								<?php 
+									}
+								?>
+																
+							</table>
+							<table class="table table-striped">
+								<tr>
+									<th>Skill Name</th>
+									<th>Skill Description</th>
+									<th></th>
+									<th></th>
+								</tr>
+
+								<?php 
+										include 'connection.php';
+
+										$q = "SELECT * FROM skill Where job_seeker_id = '".$_SESSION['job_seeker_id']."'";
+										
+										$query = mysqli_query($conn,$q);
+
+										while ($res= mysqli_fetch_array($query)) {
+										
+								?>
+
+								<tr>
+									<td><?php echo $res['skill_name']; ?></td>
+									<br>
+									<td><?php echo $res['skill_description']; ?></td>
+									<td><a class="btn btn-success" name="update" href=" ?job_seeker_id=<?php echo $res['job_seeker_id']; ?>">Update</a></td>
+									<td><a class="btn btn-success" href=" ?job_seeker_id=<?php echo $res['job_seeker_id']; ?>">Delete</a></td>
+
+								</tr>
+
+								<?php 
+									}
+								?>
+																
+							</table>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
---
+++ job_seeker_login/update_job_seeker.php
@@ -0,0 +1,99 @@
+<?php 	
+if (!isset($_SESSION)) session_start();
+?>
+<?php 	
+	include 'connection.php';
+	if (isset($_POST['submit'])) {
+
+		$job_seeker_id	=$_GET['job_seeker_id'];
+
+		$job_seeker_name 		= mysqli_real_escape_string($conn,$_POST['job_seeker_name']);
+		$job_seeker_email 		= mysqli_real_escape_string($conn,$_POST['job_seeker_email']);
+		$job_seeker_contact 	= mysqli_real_escape_string($conn,$_POST['job_seeker_contact']);
+		$job_seeker_address 	= mysqli_real_escape_string($conn,$_POST['job_seeker_address']);
+		$job_seeker_career_objective 	= mysqli_real_escape_string($conn,$_POST['job_seeker_career_objective']);
+		$job_seeker_gender 	= mysqli_real_escape_string($conn,$_POST['job_seeker_gender']);
+		$job_seeker_image 	= mysqli_real_escape_string($conn,$_POST['job_seeker_image']);
+
+		$sql= "SELECT * FROM job_seeker WHERE job_seeker_name='$job_seeker_name' AND job_seeker_address='$job_seeker_address' AND job_seeker_career_objective='$job_seeker_career_objective' AND job_seeker_gender='$job_seeker_gender'AND image='$image' AND job_seeker_id!='".$_SESSION['job_seeker_id']."'";
+		$res_s= mysqli_query($conn,$sql) or die(mysqli_error($conn));
+
+		if (mysqli_num_rows($res_s)>0) {
+			$msg= "Error !!";
+		}
+		else{
+
+			$sql= "UPDATE job_seeker SET job_seeker_id=$job_seeker_id, job_seeker_name='$job_seeker_name', job_seeker_email='$job_seeker_email', job_seeker_contact='$job_seeker_contact', job_seeker_address='$job_seeker_address', job_seeker_career_objective='$job_seeker_career_objective', job_seeker_gender='$job_seeker_gender', image='$image' WHERE job_seeker_id='".$_SESSION['job_seeker_id']."'";
+
+			$qry= mysqli_query($conn,$sql);
+
+			header("location: ");
+			}
+	}
+
+?>
+	
+		<div class="row">
+			<div class="col-md-3"></div>
+
+			<div class="col-md-6">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h3>Update Account Info.</h3>
+					</div>
+					<div class="panel-body">
+						<div style="max-width: 400px; margin: 0 auto;">
+
+							<form action="" method="POST">
+
+								<?php
+									
+
+									//$job_seeker_id	=$_GET['job_seeker_id'];
+									$q = "SELECT * FROM job_seeker WHERE job_seeker_id = '".$_SESSION['job_seeker_id']."' ";
+									$query= mysqli_query($conn,$q);
+									$res= mysqli_fetch_assoc($query);
+									
+								?>
+
+								<div class="form-group">
+									<label for="job_seeker_name">Full Name: </label>
+									<input type="text" name="job_seeker_name" id="job_seeker_name" class="form-control" required="" value="<?php echo $res['job_seeker_name']; ?>" />
+									<?php if (isset($msg)): ?>
+									<span><?php echo $msg;?></span>
+								<?php endif ?>
+								</div>								
+								<div class="form-group">
+									<label for="job_seeker_email">Email Address: </label>
+									<input type="text" name="job_seeker_email" id="job_seeker_email" class="form-control" required="" value="<?php echo $res['job_seeker_email']; ?>"/>
+								</div>
+								<div class="form-group">
+									<label for="job_seeker_contact">Contact: </label>
+									<input type="text" name="job_seeker_contact" id="job_seeker_contact" class="form-control" required="" value="<?php echo $res['job_seeker_contact']; ?>"/>
+								</div>
+								<div class="form-group">
+									<label for="job_seeker_address">Address: </label>
+									<textarea type="text" name="job_seeker_address" id="job_seeker_address" class="form-control" rows="3" ><?php echo $res['job_seeker_address']; ?></textarea>
+								</div>
+								<div class="form-group">
+									<label for="job_seeker_career_objective">Career Objective: </label>
+									<textarea type="text" name="job_seeker_career_objective" id="job_seeker_career_objective" class="form-control" rows="3" ><?php echo $res['job_seeker_career_objective']; ?></textarea>
+								</div>
+								<div class="form-group">
+									<label for="job_seeker_gender">Gender: </label>
+									<input type="text" name="job_seeker_gender" id="job_seeker_gender" class="form-control" value="<?php echo $res['job_seeker_gender']; ?>"/>
+								</div>
+								<div class="form-group">									
+									<input type="" name="image" id="image" class="form-control" value="<?php echo $res['image']; ?>"/>
+								</div>
+								
+								<button type="submit" name="submit" class="btn btn-success">Submit</button>
+							</form>
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-3"></div>
+		</div>
