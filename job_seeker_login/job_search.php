8a7be67135b0e381bdabfdece40b56ec5f8f0722
Md. Rakibur Rahman <rakiburrimon@gmail.com>
2018-12-01 21:02:29 +0600

    job details in job seeker

---
+++ job_seeker_login/job_search.php
@@ -0,0 +1,62 @@
+<?php   
+if (!isset($_SESSION)) session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search results</title>
+    <link rel="stylesheet" href="assets/css/style4.css">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "job_portal";
+
+
+$_SESSION['search_value']=$_POST["query"];
+$conn=new mysqli($servername,$username,$password,$dbname);
+if($conn->connect_error){
+    echo 'Connection Faild: '.$con->connect_error;
+    }else{
+        $sql="select * from job where job_title like '%".$_SESSION['search_value']."%'";
+
+        $quer = mysqli_query($conn,$sql);
+
+        while ($res= mysqli_fetch_array($quer)){
+            
+            ?>
+        <div class="container">
+        <div class="media border p-6 col-md-10 border border-success"">
+            <ul class="list-unstyled">
+                <li class="media">
+                    <div class="media-body">
+                        <h5 class="mt-0 mb-1"><?php echo $res['job_title']; ?></h5>
+                        <p><?php echo $res['job_context']; ?></p>
+                        <p><?php echo $res['educaqtional_requirement']; ?></p>
+                        <td><a class="btn btn-success" name="Details" href=" ?job_id=<?php echo $res['job_id']; ?>">Details..</a></td>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        </div>
+            
+    <?php
+        }
+        }       
+    ?>
+</body>
+</html>
--- job_seeker_login/welcome.php
+++ job_seeker_login/welcome.php
@@ -4,13 +4,81 @@
    
    <head>
       <title>Welcome </title>
-   </head>
-   <?php
-   include "header.php"
-   ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/passwordvalidation.css">
+    <link rel="stylesheet" href="assets/css/style4.css">
+    
+    <meta charset="utf-8">
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap/css/simple-sidebar.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style5.css">
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="../assets/css/style4.css">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+   
    <body>
-   	<h1>Welcome</h1>
-      <h2><a href = "logout.php">Sign Out</a></h2>
-   </body>
+  <body>
+  <!-- Navbar -->
+  <nav>
+   <?php include "header.php"; ?>
+  </nav>
+  <!-- End Navbar -->
+  <div>
+    
+    <div class="container text-center">
+      <div class="col-md-8 ml-auto mr-auto">
+        <div class="brand">
+          <h1 class="title">
+            Search Job
+          </h1>
+          <br/>
+          <div class="row">
+          <div class="col-12">
+            <div class="input-group-sm mb-3">
+              <form action="search.php" method="POST">
+                <input name="query" class="form-control border-secondary py-1" type="text" placeholder="Search">
+                <?php echo "<br>"; ?>
+                <div class="input-group-append">
+                    <button class="btn btn-primary btn-round btn-md col-sm-8" type="submit" value="Search">Search
+                    </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/jquery.min.js"></script>
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Chart JS -->
+  <script src="../assets/js/plugins/chartjs.min.js"></script>
+  <!--  Notifications Plugin    -->
+  <script src="../assets/js/plugins/bootstrap-notify.js"></script>
+  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="../assets/js/now-ui-dashboard.min.js?v=1.1.0" type="text/javascript"></script>
+  <!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
+  <script src="../assets/demo/demo.js"></script>
+</body>
    
 </html>
